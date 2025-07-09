bf18f0edbba2d5a222ed3006956d8219aacde961
Vaibhav Tripathi <tripathivaibhav678@gmail.com>
2025-07-09 09:00:03 +0530

    Day 11 - Chat memory + new UI

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import wikipedia
 
 app = Flask(__name__)
+chat_history = []
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    answer = ""
-    if request.method == "POST":
-        query = request.form.get("question")
-        try:
-            answer = wikipedia.summary(query, sentences=2)
-        except wikipedia.exceptions.DisambiguationError:
-            answer = "Too many results. Please be specific."
-        except wikipedia.exceptions.PageError:
-            answer = "Nothing found on that topic."
-        except:
-            answer = "Something went wrong."
-    return render_template("index.html", answer=answer)
+@app.route("/")
+def home():
+    return render_template("index.html", chat=chat_history)
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_input = request.form["message"]
+    try:
+        answer = wikipedia.summary(user_input, sentences=2)
+    except:
+        answer = "Sorry, I couldn't find an answer."
+
+    chat_history.append({"you": user_input, "tri": answer})
+    return render_template("index.html", chat=chat_history)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=True)
